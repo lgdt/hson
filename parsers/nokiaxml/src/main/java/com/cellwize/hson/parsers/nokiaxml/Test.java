e1770ae27ab1ed10161d8aac94882665913a6f36
Marat Karakhanov <marat.karakhanov@cellwize.com>
2018-01-17 16:04:14 +0600

    Adding log4j2 and kafak deps

--- parsers/nokiaxml/src/main/java/com/cellwize/hson/parsers/nokiaxml/Test.java
+++ parsers/nokiaxml/src/main/java/com/cellwize/hson/parsers/nokiaxml/Test.java
@@ -1,7 +1,13 @@
 package com.cellwize.hson.parsers.nokiaxml;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Test {
+
+  private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    logger.error("Hello, World");
   }
 }
