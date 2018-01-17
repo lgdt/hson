20c87a64e8a0e72da076a9baea71b6ffba8ff88a
Marat Karakhanov <marat.karakhanov@cellwize.com>
2018-01-17 18:06:22 +0600

    Fixing Maven projects settings

--- parsers/nokiaxml/src/main/java/com/cellwize/hson/parsers/nokiaxml/Test.java
+++ parsers/nokiaxml/src/main/java/com/cellwize/hson/parsers/nokiaxml/Test.java
@@ -3,6 +3,9 @@ package com.cellwize.hson.parsers.nokiaxml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Test {
 
   private static final Logger logger = LoggerFactory.getLogger(Test.class);
